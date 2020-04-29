8098fdcfea56684e19136d7e339d31b9ddc8c2ac
Zhi Wen <zwen8@beluga4.int.ets1.calculquebec.ca>
2020-04-29 19:38:29 -0400

    add job name

--- code/METM/submit_job.sh
+++ code/METM/submit_job.sh
@@ -1,2 +1,2 @@
 export PYTHONUNBUFFERED=1
-sbatch --export=ALL --account=ctb-liyue --gres=gpu:1 -c 1 --qos=high --mem=30G ./run.sh
+sbatch --export=ALL -J metm --account=ctb-liyue --gres=gpu:1 -c 1 --qos=high --mem=30G ./run.sh
