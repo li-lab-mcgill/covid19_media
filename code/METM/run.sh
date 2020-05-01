650490650f741043e2442ae714220afb6927d3c8
Zhi Wen <zwen8@beluga4.int.ets1.calculquebec.ca>
2020-05-01 14:33:52 -0400

    fix data path error

--- code/METM/run.sh
+++ code/METM/run.sh
@@ -3,7 +3,7 @@
 python main.py \
     --mode train \
     --dataset "GPHIN" \
-    --data_path data/GPHIN \
+    --data_path data/GPHIN/min_df_10/ \
     --num_topics 10 \
     --train_embeddings 1 \
     --epochs 10
