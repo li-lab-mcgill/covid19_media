83ff603c3bcbde4805eb8378ea0072d65f3782c4
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-13 15:12:01 -0400

    update source embedding function

--- code/DMETM/data.py
+++ code/DMETM/data.py
@@ -144,4 +144,4 @@ def get_rnn_input(tokens, counts, times, num_times, sources, vocab_size, num_doc
     return rnn_input
 
 def get_source_embeddings(path):
-    return np.load(os.path.join(path, 'source_matrix.npy'))
+    return np.load(path)
