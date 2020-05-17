354898a3ea5acfd09c83a3098b406d486ae7161a
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-17 15:59:03 -0400

    add email notification for beluga

--- code/DMETM/submit_job.sh
+++ code/DMETM/submit_job.sh
@@ -1,2 +1,11 @@
 export PYTHONUNBUFFERED=1
-sbatch --export=ALL -J metm --account=ctb-liyue --gres=gpu:1 -c 1 --qos=high --mem=30G ./run.sh
+sbatch --export=ALL \
+    -J metm \
+    --account=ctb-liyue \
+    --gres=gpu:1 \
+    -c 1 \
+    --qos=high \
+    --mem=30G \
+    --mail-type=ALL \
+    --mail-user=zhi.wen@mail.mcgill.ca \
+    ./run.sh
