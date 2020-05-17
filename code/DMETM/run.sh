7508a320c916d17ed83dbdd0023aa1a146bd8da2
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-17 15:39:46 -0400

    optimizing for memory issue

--- code/DMETM/run.sh
+++ code/DMETM/run.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-python main.py \
+CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode train \
-    --dataset "GPHIN" \
-    --data_path data/GPHIN/min_df_10/ \
-    --num_topics 10 \
-    --train_embeddings 1 \
-    --epochs 10
+    --dataset "Aylien" \
+    --data_path data/Aylien/ \
+    --emb_path skipgram/trained_word_emb_aylien.txt \
+    --save_path ./results \
+    --batch_size 8 \
+    --min_df 100 \
+    --train_source_embeddings 0 \
+    --eta_nlayers 2 \
+    --eta_hidden_size 128 \
