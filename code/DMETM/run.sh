ec9e08946f03d3f2ce19cf2ca5475f92278e5fc1
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-17 23:10:21 -0400

    try run with extremely small model

--- code/DMETM/run.sh
+++ code/DMETM/run.sh
@@ -6,10 +6,12 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --data_path data/Aylien/ \
     --emb_path skipgram/trained_word_emb_aylien.txt \
     --save_path ./results \
+    --epochs 100 \
+    --num_topics 5 \
     --batch_size 8 \
     --min_df 100 \
     --train_source_embeddings 0 \
     --eta_nlayers 2 \
-    --eta_hidden_size 128 \
-    --t_hidden_size 256 \
+    --eta_hidden_size 5 \
+    --t_hidden_size 5 \
     --eval_batch_size 100 \
