ae3cfedf7859e097230d1e640631b860175b102c
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-21 14:42:47 -0400

    debugging

--- code/DMETM/run.sh
+++ code/DMETM/run.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=9 python main.py \
     --mode train \
     --dataset "Aylien" \
     --data_path data/Aylien/ \
-    --emb_path skipgram/trained_word_emb_aylien.txt \
+    --emb_path data/trained_word_emb_aylien.txt \
     --save_path ./results \
     --epochs 100 \
-    --num_topics 50 \
-    --batch_size 8 \
+    --num_topics 10 \
+    --batch_size 16 \
     --min_df 100 \
     --train_source_embeddings 0 \
-    --eta_nlayers 2 \
-    --eta_hidden_size 5 \
-    --t_hidden_size 5 \
+    --eta_nlayers 3 \
+    --eta_hidden_size 64 \
+    --t_hidden_size 64 \
     --eval_batch_size 100 \
