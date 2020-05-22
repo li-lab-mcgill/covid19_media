62c85c0816fc3f6eb12dd84feb189ba723fe4072
Yue Li <liyue@mit.edu>
2020-05-22 14:43:35 -0400

    added training script on compute canada

---
+++ code/DMETM/train_cc.sh
@@ -0,0 +1,17 @@
+CUDA_VISIBLE_DEVICES=0 python main.py \
+    --mode train \
+    --dataset Aylien \
+    --data_path data/Aylien/ \
+    --emb_path /home/liyue/projects/ctb-liyue/data/covid19_news/trained_word_emb_aylien.txt \
+    --save_path /home/liyue/projects/ctb-liyue/liyue/Projects/covid19_media/results/dmetm \
+    --lr 1e-3 \
+    --clip 2.0 \
+    --epochs 20 \
+    --num_topics 50 \
+    --batch_size 64 \
+    --min_df 100 \
+    --train_source_embeddings 0 \
+    --eta_nlayers 3 \
+    --eta_hidden_size 512 \
+    --t_hidden_size 512 \
+    --eval_batch_size 100
