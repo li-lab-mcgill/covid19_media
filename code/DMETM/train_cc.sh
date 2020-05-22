bbfe2312191b1d9b5635b51d6cdc51f474d14f41
Yue Li <liyue@mit.edu>
2020-05-22 14:49:39 -0400

    added training script on compute canada

--- code/DMETM/train_cc.sh
+++ code/DMETM/train_cc.sh
@@ -1,7 +1,15 @@
+#!/bin/bash
+
+#SBTACH --time=1:0:0 
+#SBATCH --ntasks=1 
+#SBATCH --account=ctb-liyue
+#SBATCH --gres=gpu:1
+#SBATCH --mem=125G
+
 CUDA_VISIBLE_DEVICES=0 python main.py \
     --mode train \
     --dataset Aylien \
-    --data_path data/Aylien/ \
+    --data_path /home/liyue/projects/ctb-liyue/liyue/Projects/covid19_media/data/Aylien \
     --emb_path /home/liyue/projects/ctb-liyue/data/covid19_news/trained_word_emb_aylien.txt \
     --save_path /home/liyue/projects/ctb-liyue/liyue/Projects/covid19_media/results/dmetm \
     --lr 1e-3 \
@@ -15,3 +23,7 @@ CUDA_VISIBLE_DEVICES=0 python main.py \
     --eta_hidden_size 512 \
     --t_hidden_size 512 \
     --eval_batch_size 100
+
+
+# python main.py --min_df 100 --dataset Aylien --data_path /home/liyue/projects/ctb-liyue/liyue/Projects/covid19_media/data/Aylien --num_topics 50 --t_hidden_size 64 --eta_hidden_size 64 --batch_size 100 --epochs 100 --emb_path /home/liyue/projects/ctb-liyue/data/covid19_news/trained_word_emb_aylien.txt --eval_batch_size 100 --save_path /home/liyue/projects/ctb-liyue/liyue/Projects/covid19_media/results/dmet --clip 2.0
+
