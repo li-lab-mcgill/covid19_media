ec720984ad0a9e9241fc7a4d7a5a384af72bda7a
Zhi <zhiwen120@gmail.com>
2020-05-25 11:00:08 -0400

    update job submission script

--- code/DMETM/submit_job.sh
+++ code/DMETM/submit_job.sh
@@ -1,9 +1,11 @@
 export PYTHONUNBUFFERED=1
 sbatch --export=ALL \
     -J metm \
+    --time=30:00:00 \
     --account=ctb-liyue \
     --gres=gpu:1 \
-    -c 1 \
+    --output slurm_output/slurm-%j.out \
+    -c 2 \
     --qos=high \
     --mem=30G \
     --mail-type=ALL \
