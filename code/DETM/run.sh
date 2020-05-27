4289854ff7caf5de0bf0c13a25bbb298fa57b1ae
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-27 15:56:11 -0400

    debug

--- code/DETM/run.sh
+++ code/DETM/run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-CUDA_VISIBLE_DEVICES=0 python main.py \
+CUDA_VISIBLE_DEVICES=9 python main.py \
     --mode train \
-    --dataset "Aylien" \
-    --data_path data/Aylien/ \
-    --emb_path data/trained_word_emb_aylien.txt \
+    --dataset "GPHIN" \
+    --data_path data/GPHIN/ \
+    --emb_path data/skipgram_emb_300d.txt \
     --save_path ./results \
     --lr 1e-3 \
     --clip 2 \
     --enc_drop 0.1 \
     --eta_dropout 0.1 \
-    --epochs 40 \
+    --epochs 100 \
     --num_topics 50 \
-    --batch_size 32 \
-    --min_df 100 \
+    --batch_size 64 \
+    --min_df 10 \
     --eta_nlayers 3 \
     --eta_hidden_size 512 \
     --t_hidden_size 512 \
