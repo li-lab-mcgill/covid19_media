1be9951428339b968056a613c089453a486f37a9
Yue Li <liyue@mit.edu>
2020-05-27 10:46:43 -0400

    files restored

--- code/DMETM/data.py
+++ code/DMETM/data.py
@@ -32,34 +32,42 @@ def _fetch(path, name):
     return {'tokens': tokens, 'counts': counts}
 
 def _fetch_temporal(path, name):
+    
     if name == 'train':
-        token_file = os.path.join(path, 'bow_tr_tokens.mat')
-        count_file = os.path.join(path, 'bow_tr_counts.mat')
-        time_file = os.path.join(path, 'bow_tr_timestamps.mat')
+        token_file = os.path.join(path, 'bow_tr_tokens')
+        count_file = os.path.join(path, 'bow_tr_counts')
+        time_file = os.path.join(path, 'bow_tr_timestamps')
+        source_file = os.path.join(path, 'bow_tr_sources.pkl')
     elif name == 'valid':
-        token_file = os.path.join(path, 'bow_va_tokens.mat')
-        count_file = os.path.join(path, 'bow_va_counts.mat')
-        time_file = os.path.join(path, 'bow_va_timestamps.mat')
+        token_file = os.path.join(path, 'bow_va_tokens')
+        count_file = os.path.join(path, 'bow_va_counts')
+        time_file = os.path.join(path, 'bow_va_timestamps')
+        source_file = os.path.join(path, 'bow_va_sources.pkl')
     else:
-        token_file = os.path.join(path, 'bow_ts_tokens.mat')
-        count_file = os.path.join(path, 'bow_ts_counts.mat')
-        time_file = os.path.join(path, 'bow_ts_timestamps.mat')
+        token_file = os.path.join(path, 'bow_ts_tokens')
+        count_file = os.path.join(path, 'bow_ts_counts')
+        time_file = os.path.join(path, 'bow_ts_timestamps')
+        source_file = os.path.join(path, 'bow_ts_sources.pkl')
+    
     tokens = scipy.io.loadmat(token_file)['tokens'].squeeze()
     counts = scipy.io.loadmat(count_file)['counts'].squeeze()
     times = scipy.io.loadmat(time_file)['timestamps'].squeeze()
+    sources = np.array(pickle.load(open(source_file, 'rb')))
+
     if name == 'test':
-        token_1_file = os.path.join(path, 'bow_ts_h1_tokens.mat')
-        count_1_file = os.path.join(path, 'bow_ts_h1_counts.mat')
-        token_2_file = os.path.join(path, 'bow_ts_h2_tokens.mat')
-        count_2_file = os.path.join(path, 'bow_ts_h2_counts.mat')
+        token_1_file = os.path.join(path, 'bow_ts_h1_tokens')
+        count_1_file = os.path.join(path, 'bow_ts_h1_counts')
+        token_2_file = os.path.join(path, 'bow_ts_h2_tokens')
+        count_2_file = os.path.join(path, 'bow_ts_h2_counts')        
         tokens_1 = scipy.io.loadmat(token_1_file)['tokens'].squeeze()
         counts_1 = scipy.io.loadmat(count_1_file)['counts'].squeeze()
         tokens_2 = scipy.io.loadmat(token_2_file)['tokens'].squeeze()
         counts_2 = scipy.io.loadmat(count_2_file)['counts'].squeeze()
-        return {'tokens': tokens, 'counts': counts, 'times': times, 
+        return {'tokens': tokens, 'counts': counts, 'times': times, 'sources': sources,
                     'tokens_1': tokens_1, 'counts_1': counts_1, 
                         'tokens_2': tokens_2, 'counts_2': counts_2} 
-    return {'tokens': tokens, 'counts': counts, 'times': times}
+
+    return {'tokens': tokens, 'counts': counts, 'times': times, 'sources': sources}
 
 def get_data(path, temporal=False):
     ### load vocabulary
@@ -77,19 +85,29 @@ def get_data(path, temporal=False):
 
     return vocab, train, valid, test
 
-def get_batch(tokens, counts, ind, vocab_size, emsize=300, temporal=False, times=None):
+def get_batch(tokens, counts, ind, vocab_size, sources, emsize=300, temporal=False, times=None):
+    
     """fetch input data by batch."""
     batch_size = len(ind)
     data_batch = np.zeros((batch_size, vocab_size))
+    
     if temporal:
         times_batch = np.zeros((batch_size, ))
+
+    sources_batch = np.zeros((batch_size, ))
+
     for i, doc_id in enumerate(ind):
+        
         doc = tokens[doc_id]
         count = counts[doc_id]
+        
+        source = sources[doc_id]
+        sources_batch[i] = source
+
         if temporal:
             timestamp = times[doc_id]
             times_batch[i] = timestamp
-        L = count.shape[1]
+        
         if len(doc) == 1: 
             doc = [doc.squeeze()]
             count = [count.squeeze()]
@@ -99,19 +117,23 @@ def get_batch(tokens, counts, ind, vocab_size, emsize=300, temporal=False, times
         if doc_id != -1:
             for j, word in enumerate(doc):
                 data_batch[i, word] = count[j]
+    
     data_batch = torch.from_numpy(data_batch).float().to(device)
+    sources_batch = torch.from_numpy(sources_batch).to(device)
+
     if temporal:
         times_batch = torch.from_numpy(times_batch).to(device)
-        return data_batch, times_batch
-    return data_batch
+        return data_batch, times_batch, sources_batch
 
-def get_rnn_input(tokens, counts, times, num_times, vocab_size, num_docs):
+    return data_batch, sources_batch
+
+def get_rnn_input(tokens, counts, times, num_times, sources, vocab_size, num_docs):
     indices = torch.randperm(num_docs)
-    indices = torch.split(indices, 1000) 
+    indices = torch.split(indices, 1000)
     rnn_input = torch.zeros(num_times, vocab_size).to(device)
     cnt = torch.zeros(num_times, ).to(device)
     for idx, ind in enumerate(indices):
-        data_batch, times_batch = get_batch(tokens, counts, ind, vocab_size, temporal=True, times=times)
+        data_batch, times_batch, sources_batch = get_batch(tokens, counts, ind, vocab_size, sources, temporal=True, times=times)
         for t in range(num_times):
             tmp = (times_batch == t).nonzero()
             docs = data_batch[tmp].squeeze().sum(0)
@@ -121,3 +143,7 @@ def get_rnn_input(tokens, counts, times, num_times, vocab_size, num_docs):
             print('idx: {}/{}'.format(idx, len(indices)))
     rnn_input = (rnn_input + 1e-16) / (cnt.unsqueeze(1) + 1e-16)
     return rnn_input
+
+def get_source_embeddings(path):
+    return torch.tensor(np.load(path), dtype=torch.float32)
+
