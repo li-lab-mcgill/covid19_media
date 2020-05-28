5b521bb3ac9ba738b7e19078b5c6abaf5b68918f
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-28 13:36:14 -0400

    5000 epochs

--- code/DETM/run.sh
+++ code/DETM/run.sh
@@ -9,7 +9,7 @@ CUDA_VISIBLE_DEVICES=7 python main.py \
     --clip 2 \
     --enc_drop 0.1 \
     --eta_dropout 0.1 \
-    --epochs 100 \
+    --epochs 500 \
     --num_topics 50 \
     --batch_size 64 \
     --min_df 10 \
@@ -17,5 +17,5 @@ CUDA_VISIBLE_DEVICES=7 python main.py \
     --eta_hidden_size 512 \
     --t_hidden_size 512 \
     --eval_batch_size 100 \
-    --train_embeddings 1 \
+    --train_embeddings 0 \
     --emb_path data/skipgram_emb_300d.txt \
