4c48fcee2d827adf123333158e727e9fa8680fc2
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-05-28 12:03:57 -0400

    100 epochs

--- code/DETM/run.sh
+++ code/DETM/run.sh
@@ -4,13 +4,12 @@ CUDA_VISIBLE_DEVICES=7 python main.py \
     --mode train \
     --dataset "GPHIN" \
     --data_path data/GPHIN/ \
-    --emb_path data/skipgram_emb_300d.txt \
     --save_path ./results \
     --lr 1e-3 \
     --clip 2 \
     --enc_drop 0.1 \
     --eta_dropout 0.1 \
-    --epochs 10 \
+    --epochs 100 \
     --num_topics 50 \
     --batch_size 64 \
     --min_df 10 \
@@ -18,3 +17,5 @@ CUDA_VISIBLE_DEVICES=7 python main.py \
     --eta_hidden_size 512 \
     --t_hidden_size 512 \
     --eval_batch_size 100 \
+    --train_embeddings 1 \
+    --emb_path data/skipgram_emb_300d.txt \
