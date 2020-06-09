8db678bc674cdc9d8511019b1c124345b74a1974
ImaneChafi <imane.chafi@mail.mcgill.ca>
2020-06-09 16:24:27 -0400

    Testing

--- code/DMETM/data/GPHIN/pkl_to_txt.py
+++ code/DMETM/data/GPHIN/pkl_to_txt.py
@@ -1,8 +1,6 @@
 import pickle
 
-infile = open('min_df_10/bow_tr_ids.pkl','rb')
-new_dict = pickle.load(infile)
-f = open("ids_map.txt", "a")
-f.write(str(new_dict))
-f.close()
-infile.close()
+
+with open('min_df_10/vocab.pkl', 'rb') as f:
+    data = pickle.load(f)
+    print(data)
