19ac025ebcdf1e36daccf1db482e4767462872eb
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-06-09 23:34:27 -0400

    debug

--- code/ETM/main.py
+++ code/ETM/main.py
@@ -216,7 +216,6 @@ def visualize(m, show_emb=True):
         print('Visualize topics...')
         topics_words = []
         gammas = m.get_beta()
-        raise Exception(gammas.shape)
         for k in range(args.num_topics):
             gamma = gammas[k]
             top_words = list(gamma.cpu().numpy().argsort()[-args.num_words+1:][::-1])
@@ -360,6 +359,7 @@ with torch.no_grad():
 
     ## show topics
     beta = model.get_beta()
+    raise Exception(beta.shape)
     topic_indices = list(np.random.choice(args.num_topics, 10)) # 10 random topics
     print('\n')
     for k in range(args.num_topics):#topic_indices:
