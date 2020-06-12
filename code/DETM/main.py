15fd5c5e18f964c2f2a7ba37a09e69b3fd88cf6e
Zhi WEN <zwen8@nahanni.CS.McGill.CA>
2020-06-12 14:45:06 -0400

    bug fix

--- code/DETM/main.py
+++ code/DETM/main.py
@@ -547,8 +547,8 @@ def get_topic_quality():
             # TC_all.append(tc)
             TC_all[tt] = tc
             cnt_all.append(cnt)
-        TC_all = TC_all / cnt_all[0].item()
-        TC_all[TC_all<0] = 0
+        TC_all = TC_all / cnt_all[0]
+        # TC_all[TC_all<0] = 0
         print('TC_all: ', TC_all)
         # TC_all = torch.tensor(TC_all)
         print('TC_all: ', TC_all.size)
